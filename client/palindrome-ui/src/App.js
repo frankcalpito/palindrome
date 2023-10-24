f8021c80d554bc17aaafd6ba3fd0ec229d651b3b
Frank Calpito <fccalpitojr@gmail.com>
2023-10-24 16:35:36 -0600

    - add button icon and icon styling

--- client/palindrome-ui/src/App.js
+++ client/palindrome-ui/src/App.js
@@ -4,6 +4,7 @@ import palindromeService from "./services/palindromeService";
 import Input from "./components/Input/Input";
 import Button from "./components/Button/Button";
 
+import { ReactComponent as SearchSVG } from "./assets/images/magnifying-glass.svg";
 
 function App() {
   const [number, setNumber] = useState();
@@ -32,6 +33,7 @@ function App() {
         onClick={handleFindClosestPalindromeClick}
         loading={loading}
         label="Find closest palindrome"
+        Icon={SearchSVG}
         aria-label="Find closest palindrome" />
       <div className={['result', result ? 'show' : null].join(' ')}>
         <span>The answer is:</span>
